b48362bd8f8fab7cd4455bd6ee40728dd76ac68d
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2022-01-06 20:12:43 -0500

    Formatted code to match PEP8

--- 009-Secret-Auction/secret_auction.py
+++ 009-Secret-Auction/secret_auction.py
@@ -1,21 +1,28 @@
 from logo import logo
 
+
 def secret_auction():
     bids = accept_bids()
     find_highest_bid(bids)
+
 
 def accept_bids(bids={}):
     print(logo)
     name = input("What is your name: ")
     amount = int(input("What is your bid: $"))
     bids[name] = amount
-    continue_bid = input("Are there any other players? Please type 'yes' or 'no'").lower()
+    continue_bid = input(
+        "Are there any other players? Please type 'yes' or 'no'"
+    ).lower()
     if continue_bid == "yes":
         return accept_bids(bids)
     else:
         return bids
 
-def find_highest_bid(bids, highest_bidder=None, highest_bid=float("-inf")):
+
+def find_highest_bid(
+    bids, highest_bidder=None, highest_bid=float("-inf")
+):
     for bidder in bids:
         bid = bids[bidder]
         if bid > highest_bid:
@@ -23,7 +30,8 @@ def find_highest_bid(bids, highest_bidder=None, highest_bid=float("-inf")):
             highest_bid = bid
     print(f"The winner is {highest_bidder} with a bid of {highest_bid}!")
 
-if __name__ == "__main__":
+
+def main():
     play_again = True
     while play_again:
         secret_auction()
@@ -31,3 +39,7 @@ if __name__ == "__main__":
         if restart_game == "no":
             play_again = False
     print("Thanks for participating in the blind auction!")
+
+
+if __name__ == "__main__":
+    main()
